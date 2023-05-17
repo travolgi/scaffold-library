4a7ae8420592aaf2a6f05509ad336283ddd76eba
Travolgi <travolgi@gmail.com>
2023-05-17 15:24:47 +0200

    commit add hook for scrool animations

---
+++ hooks/useElementOnScreen.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+function useElementOnScreen(ref, rootMargin='0px') {
+	const [isIntersecting, setIsIntersecting] = useState(true);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				setIsIntersecting(entry.isIntersecting);
+			},
+			{ rootMargin }
+		);
+		if (ref.current) observer.observe(ref.current);
+		
+		return () => {
+			if (ref.current) observer.unobserve(ref.current);
+		}
+	}, []);
+	return isIntersecting;
+}
+
+export default useElementOnScreen;
